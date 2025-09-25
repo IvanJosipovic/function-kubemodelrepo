8c10b44a00732d949de50e0e6a9231f624473464
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-09-25 10:39:47 -0700

    fix: update SourceGenerator to v1.0.1 (#11)

--- src/Function/RunFunctionService.cs
+++ src/Function/RunFunctionService.cs
@@ -210,7 +210,7 @@ public class RunFunctionService(ILogger<RunFunctionService> logger) : FunctionRu
                     <Project>
                         <ItemGroup>
                             <PackageReference Include="KubernetesClient" Version="17.0.14" />
-                            <PackageReference Include="KubernetesCRDModelGen.SourceGenerator" Version="1.0.0-alpha.621">
+                            <PackageReference Include="KubernetesCRDModelGen.SourceGenerator" Version="1.0.1">
                                 <PrivateAssets>all</PrivateAssets>
                             </PackageReference>
                         </ItemGroup>
@@ -427,3 +427,4 @@ public static class Extensions
         resp.Desired.AddOrUpdate(key, newFile);
     }
 }
+
